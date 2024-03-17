0015245f6b3278c5fbd6a0518dbbc933b651ea10
mogomez967 <83328456+mogomez967@users.noreply.github.com>
2024-03-17 00:52:54 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+var client_id = 'CLIENT_ID';
+var client_secret = 'CLIENT_SECRET';
+
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+
+request.post(authOptions, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+    var token = body.access_token;
+  }
+});
+
+
+async function getProfile(accessToken) {
+  let accessToken = localStorage.getItem('access_token');
+
+  const response = await fetch('https://api.spotify.com/v1/me', {
+    headers: {
+      Authorization: 'Bearer ' + accessToken
+    }
+  });
+
+  const data = await response.json();
+}
